be3ca7b3120dc6f226bb89208cca3c1177176acd
Saksham Sahgal <73836766+SakshamSahgal@users.noreply.github.com>
2023-10-04 15:11:13 +0530

    got login data from client

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-const app = require('express')()
+const express = require('express')
+const app = express()
 const path = require("path");
 const mqttSubscriber = require('./mqtt.js')
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
+
+// Parse JSON and URL-encoded data
+app.use(express.json()); // To support JSON-encoded bodies
+app.use(express.urlencoded({ extended: false }));
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
@@ -11,4 +17,12 @@ app.listen(3000, () => {
 
 app.get('/', (req, res) => {
     res.render(path.join(__dirname,"ClientSide","index.ejs"))
+})
+
+app.post("/AdminLogin", (req, res) => {
+    console.log(req.body)
+})
+
+app.post("/CustomerLogin", (req, res) => {
+    console.log(req.body)
 })
